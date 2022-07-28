92f6c44f92a4d93d26dba35b0adbe327af25cfac
Harjappan Singh <singhharjappan@gmail.com>
2022-07-28 16:10:41 +0530

    added list and remove button

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
+const totalPriceElement = document.getElementById("total_price");
+const parentListElement = document.getElementById("list")
 totalPrice = 0;
 
 function addItem(form) {
     const itemName = form.item_name.value;
     const itemPrice = form.item_price.value;
-    const totalPriceElement = document.getElementById("total_price");
-    totalPrice += parseInt(itemPrice);
 
-    totalPriceElement.innerText = `Total price of items in your cart: $${totalPrice}`;
+    totalPrice += parseInt(itemPrice);
+    totalPriceElement.innerText = `Total price of items in your cart: ${totalPrice} INR`;
+
+    const listElement = document.createElement("li");
+    const listTextNode = document.createTextNode(`${itemName} - ${itemPrice} INR`);
+    listElement.classList.add("list-group-item", "d-flex", "justify-content-between");
+    listElement.appendChild(listTextNode);
+    parentListElement.appendChild(listElement);
+
+    const removeButton = document.createElement("button");
+    const removeButtonTextNode = document.createTextNode("Remove");
+    removeButton.classList.add("btn", "btn-danger");
+    removeButton.appendChild(removeButtonTextNode);
+    listElement.appendChild(removeButton);
+
+    removeButton.addEventListener("click", () => {
+        listElement.remove();
+        totalPrice -= parseInt(itemPrice);
+        totalPriceElement.innerText = `Total price of items in your cart: ${totalPrice} INR`;
+    });
 
     return false
 }
