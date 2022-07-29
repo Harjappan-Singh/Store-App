ca20305f0512244cafbd3278f80db994afc93897
Harjappan Singh <singhharjappan@gmail.com>
2022-07-29 20:34:18 +0530

    created state for the app

--- script.js
+++ script.js
@@ -1,31 +1,50 @@
 const totalPriceElement = document.getElementById("total_price");
 const parentListElement = document.getElementById("list")
-totalPrice = 0;
+let totalPrice = 0;
+let cart = [];
+
+class Item {
+    constructor(name, price) {
+        this.name = name
+        this.price = price
+    }
+}
+
+function refreshUI() {
+    totalPriceElement.innerText = `Total price of items in your cart: ${totalPrice} INR`;
+    parentListElement.innerHTML = ""
+
+    cart.forEach((item, index) => {
+        const listElement = document.createElement("li");
+        const listTextNode = document.createTextNode(`${item.name} - ${item.price} INR`);
+        listElement.classList.add("list-group-item", "d-flex", "justify-content-between");
+        listElement.appendChild(listTextNode);
+        parentListElement.appendChild(listElement);
+
+        const removeButton = document.createElement("button");
+        const removeButtonTextNode = document.createTextNode("Remove");
+        removeButton.classList.add("btn", "btn-danger");
+        removeButton.appendChild(removeButtonTextNode);
+        listElement.appendChild(removeButton);
+
+        removeButton.addEventListener("click", () => {
+            cart.splice(index, 1)
+            totalPrice -= item.price;
+            refreshUI();
+        });
+    });
+
+}
 
 function addItem(form) {
     const itemName = form.item_name.value;
     const itemPrice = form.item_price.value;
 
     totalPrice += parseInt(itemPrice);
-    totalPriceElement.innerText = `Total price of items in your cart: ${totalPrice} INR`;
+    const item = new Item(itemName, parseInt(itemPrice));
+    cart.push(item);
 
-    const listElement = document.createElement("li");
-    const listTextNode = document.createTextNode(`${itemName} - ${itemPrice} INR`);
-    listElement.classList.add("list-group-item", "d-flex", "justify-content-between");
-    listElement.appendChild(listTextNode);
-    parentListElement.appendChild(listElement);
-
-    const removeButton = document.createElement("button");
-    const removeButtonTextNode = document.createTextNode("Remove");
-    removeButton.classList.add("btn", "btn-danger");
-    removeButton.appendChild(removeButtonTextNode);
-    listElement.appendChild(removeButton);
-
-    removeButton.addEventListener("click", () => {
-        listElement.remove();
-        totalPrice -= parseInt(itemPrice);
-        totalPriceElement.innerText = `Total price of items in your cart: ${totalPrice} INR`;
-    });
+    refreshUI();
 
     return false
 }
